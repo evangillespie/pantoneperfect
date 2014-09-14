9d243553e13b84b8d0469e4b930704c6787eacf4
Evan Gillespie <evan.gillespie1@gmail.com>
2014-09-13 20:33:16 -0600

    set up serial communication

--- arduino/pantoneperfect/pantoneperfect.ino
+++ arduino/pantoneperfect/pantoneperfect.ino
@@ -8,23 +8,38 @@
 
 int neopixel_pin = 6;
 
-int num_neopixels = 120;
+int num_neopixels = 60;
 Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(num_neopixels, neopixel_pin);
 
 void setup(){
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
   led_strip.begin();
   led_strip.show();
 }
 
 void loop(){
-  for (int i=0; i < num_neopixels; i++){
-    led_strip.setPixelColor(i, led_strip.Color(30, 70, 30));
-    led_strip.show();
-    delay(10); 
+  if (Serial.available())  {
+    // expect communications in the form: RRRGGGBBB
+    char red[] = "000";
+    char green[] = "000";
+    char blue[] = "000";
+    
+    Serial.readBytes(red, 3);
+    Serial.readBytes(green, 3);
+    Serial.readBytes(blue, 3);
+    
+    light_strip(atoi(red), atoi(green), atoi(blue));
+    
   }
-  for (int i=0; i < num_neopixels; i++){
-    led_strip.setPixelColor(i, led_strip.Color(15, 40, 80));
-    led_strip.show();
-    delay(10); 
+  delay(500);
+}
+
+void light_strip(int red, int green, int blue) {
+  // TODO: validation
+  for (int i = 0; i < 10; i++) {
+    led_strip.setPixelColor(i, led_strip.Color(green, red, blue));
   }
+  led_strip.show();
 }
