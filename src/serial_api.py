e5b0e81b9c2d1f42f97aab6dfb74f6ffd3dd9f20
Evan Gillespie <evan.gillespie1@gmail.com>
2014-09-14 19:45:55 -0600

    converted indentation to spaces

--- src/serial_api.py
+++ src/serial_api.py
@@ -10,30 +10,30 @@ class SerialApi(object):
     """
 
     def __init__(self):
-    	try:
-	        self.ser = serial.Serial(SERIAL_DEVICE, 9600)
-	    except serial.serialutil.SerialException:
-	    	self.ser = serial.Serial(SERIAL_DEVICE2, 9600)
+        try:
+            self.ser = serial.Serial(SERIAL_DEVICE, 9600)
+        except serial.serialutil.SerialException:
+            self.ser = serial.Serial(SERIAL_DEVICE2, 9600)
     def send_color(self, r, g, b):
-    	"""
-    	send a colour string over the serial connection to the arduino
+        """
+        send a colour string over the serial connection to the arduino
 
-    	:param r: red component 0-255
-    	:param g: green
-    	:param b: blue
-    	"""
-    	# make sure the components are integers
-    	r = int(r)
-    	g = int(g)
-    	b = int(b)
+        :param r: red component 0-255
+        :param g: green
+        :param b: blue
+        """
+        # make sure the components are integers
+        r = int(r)
+        g = int(g)
+        b = int(b)
 
-    	if r > 255 or r < 0:
-    		raise Exception("red component out of range: %s" % r)
-    	if g > 255 or g < 0:
-    		raise Exception("green component out of range: %s" % g)
-    	if b > 255 or b < 0:
-    		raise Exception("blue component out of range: %s" % b)
-    		
-    	ser_str = '%03d%03d%03d' % (r,g,b)
+        if r > 255 or r < 0:
+            raise Exception("red component out of range: %s" % r)
+        if g > 255 or g < 0:
+            raise Exception("green component out of range: %s" % g)
+        if b > 255 or b < 0:
+            raise Exception("blue component out of range: %s" % b)
+            
+        ser_str = '%03d%03d%03d' % (r,g,b)
 
-    	self.ser(ser_str)
+        self.ser(ser_str)
