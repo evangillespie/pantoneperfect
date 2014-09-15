f3b3bee5cf431ea5711e2dafdbcdfe5713fd2502
Evan Gillespie <evan.gillespie1@gmail.com>
2014-09-14 19:35:09 -0600

    added some serial to the python

--- arduino/pantoneperfect/pantoneperfect.ino
+++ arduino/pantoneperfect/pantoneperfect.ino
@@ -12,13 +12,15 @@ int num_neopixels = 60;
 Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(num_neopixels, neopixel_pin);
 
 void setup(){
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  pinMode(13, HIGH);
+  digitalWrite(13, LOW);
   
   led_strip.begin();
   led_strip.show();
 }
 
+int state = 0;
 void loop(){
   if (Serial.available())  {
     // expect communications in the form: RRRGGGBBB
@@ -32,14 +34,30 @@ void loop(){
     
     light_strip(atoi(red), atoi(green), atoi(blue));
     
+     if ( state == 0 ){
+       state = 1;
+       digitalWrite(13, HIGH);
+     }
+     else {
+       state = 0;
+       digitalWrite(13, LOW);
+     }
   }
   delay(500);
+  
+//  light_strip(255, 0, 0);
+//  delay(5000);
+//  light_strip(0, 255, 0);
+//  delay(5000);
+//  light_strip(0, 0, 255);
+//  delay(5000);
+  
 }
 
 void light_strip(int red, int green, int blue) {
   // TODO: validation
-  for (int i = 0; i < 10; i++) {
-    led_strip.setPixelColor(i, led_strip.Color(green, red, blue));
+  for (int i = 0; i < led_strip.numPixels(); i++) {
+    led_strip.setPixelColor(i, led_strip.Color(red, green, blue));
   }
   led_strip.show();
 }
--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from sys import argv
 from PIL import Image
 
 from src.api import PPApi
+from src.serial_api import SerialApi
 
 __author__ = ('evan', )
 
@@ -20,9 +21,6 @@ def take_picture():
     api = PPApi()
     filename = api.take_picture()
 
-    img = Image.open(filename)
-    img.show()
-
 def take_and_analyze_picture():
     """
     take a picture and find the colour
@@ -30,6 +28,19 @@ def take_and_analyze_picture():
     api = PPApi()
     filename = api.take_picture()
     get_image_color(filename)
+
+def send_serial(r, g, b):
+    """
+    send an rbg colour to the arduino over serial
+    """
+    ser = SerialApi()
+    ser.send_color(r, g, b)
+
+def picture_to_arduino():
+    """
+    take a picture and send it's average colour to the arduino over serial
+    """
+    raise NotImplementedError("no serial yet")
 
 def run(interval):
     raise NotImplementedError("You have not set up the infinite loop yet")
@@ -39,6 +50,9 @@ def print_help():
     print "COMMANDS:"
     print "get_image_color <path_to_image>"
     print "take_picture"
+    print "take_and_analyze_picture"
+    print "picture_to_arduino"
+    print "send_serial <red> <green> <blue>"
     print "run <update_seconds=60>"
 
 if __name__ == '__main__':
@@ -52,6 +66,13 @@ if __name__ == '__main__':
             take_picture()
         elif command == 'take_and_analyze_picture':
             take_and_analyze_picture()
+        elif command == "picture_to_arduino":
+            picture_to_arduino()
+        elif command == "send_serial":
+            if len(argv) == 5:
+                send_serial(argv[2], argv[3], argv[4])
+            else:
+                print_help()
         elif command == 'run':
             if len(argv) == 3:
                 time = int(argv[2])
