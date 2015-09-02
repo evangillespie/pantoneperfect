825ab19aea9e1aa93a2d7d822af3bd60673089be
Evan Gillespie <evan.gillespie1@gmail.com>
2015-09-02 16:55:03 -0600

    Minor config adjustment

--- src/config.py
+++ src/config.py
@@ -6,9 +6,8 @@ PLATFORM = 'pc'
 # Should we ignore all pixels where all values are above a theshhold?
 IGNORE_BRIGHT_PIXELS=True
 
-
 # What is the threshhold for all three colors to ignore the pixels
-IGNORE_BRIGHT_THRESHHOLD=230
+IGNORE_BRIGHT_THRESHHOLD=240
 
 
 # directory in the filesyetem that images are stored in
